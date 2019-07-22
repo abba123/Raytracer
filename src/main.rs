a45d1aa95bfea806f0a1a5b8532f63d1153eeed4
abba123 <a0981861951@gmail.com>
2019-07-22 23:56:49 +0800

    define Scene

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+pub struct Color{
+    pub r: u32,
+    pub g: u32,
+    pub b :u32,
+}
+
+pub struct Sphere{
+    pub center: Point
+    pub rdius: f64,
+    pub color: Color,
+}
+
+pub struct Scene{
+    pub width: u32,
+    pub height: u32,
+    pub fov: f64,
+    pub Sphere:Sphere,
 }
