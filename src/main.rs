fed4d374e63db4acfe74272cc12bb30fd43801aa
abba123 <a0981861951@gmail.com>
2019-07-24 00:08:34 +0800

    render

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 extern crate image;
+use image::DynamicImage;
 
 pub struct Point{
-    pub x: u32,
-    pub y: u32,
-    pub z: u32
+    pub x: f32,
+    pub y: f32,
+    pub z: f32
 }
 
 pub struct Color{
-    pub r: u32,
-    pub g: u32,
-    pub b: u32,
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
 }
 
 pub struct Sphere{
     pub center: Point,
-    pub rdius: f64,
+    pub radius: f64,
     pub color: Color,
 }
 
@@ -22,9 +23,33 @@ pub struct Scene{
     pub width: u32,
     pub height: u32,
     pub fov: f64,
-    pub Sphere: Sphere,
+    pub sphere: Sphere,
+}
+
+pub fn render(scene: &Scene) -> DynamicImage{
+    DynamicImage::new_rgb8(scene.width, scene.height)
 }
 
 fn main() {
+    let scene = Scene {
+        width: 800,
+        height: 600,
+        fov: 90.0,
+        sphere: Sphere{
+            center: Point{
+                x: 0.0,
+                y: 0.0,
+                z: -5.0,
+            },
+            radius: 1.0,
+            color: Color{
+                r: 0.4,
+                g: 1.0,
+                b: 0.4,
+            },
+        },
+    };
 
+    let img:DynamicImage = render(&scene);
+    img.save("test.png").unwrap();
 }
