91ea72b373d845f118077c2fb2b1e94dd0d8cf81
abba123 <a0981861951@gmail.com>
2019-07-23 23:17:26 +0800

    add cargo

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
+extern crate image;
+
+pub struct Point{
+    pub x: u32,
+    pub y: u32,
+    pub z: u32
+}
+
 pub struct Color{
     pub r: u32,
     pub g: u32,
-    pub b :u32,
+    pub b: u32,
 }
 
 pub struct Sphere{
-    pub center: Point
+    pub center: Point,
     pub rdius: f64,
     pub color: Color,
 }
@@ -14,5 +22,9 @@ pub struct Scene{
     pub width: u32,
     pub height: u32,
     pub fov: f64,
-    pub Sphere:Sphere,
+    pub Sphere: Sphere,
+}
+
+fn main() {
+
 }
